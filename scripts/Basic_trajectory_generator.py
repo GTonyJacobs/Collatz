cac786a3fa98e903be6716cbb671fb386c160b2e
GTonyJacobs <gtjacobs@hotmail.com>
2025-02-02 00:02:54 -0600

    tidying up for updates

--- scripts/Basic_trajectory_generator.py
+++ scripts/Basic_trajectory_generator.py
@@ -4,12 +4,12 @@ def collatz_step(numer,multiplier,denom):
     numer >>= v2
     return numer,v2
     
-def collatzTrajectory(seed):
+def collatz_trajectory(seed):
     n=seed
     trajectory=[]
     v2_vector=[]
     while n>1:
-        n,v=collatzStep(n,3,1)
+        n,v=collatz_step(n,3,1)
         trajectory.append(n)
         v2_vector.append(v)
     return trajectory, v2_vector
@@ -17,10 +17,10 @@ def collatzTrajectory(seed):
 ## Enter desired starting value here
 seed=1137
 
-trajectory, v2_vector=collatzTrajectory(seed)
-L=len(trajectory)
-W=sum(v2_vector)
+trajectory, v2_vector=collatz_trajectory(seed)
+odd_steps=len(trajectory)
+even_steps=sum(v2_vector)
 print(f"trajectory of {seed}: {trajectory}")
 print(f"divisions at each step: {v2_vector}")
-print(f"number of odd steps = {L}")
-print(f"number of even steps = {W}")
+print(f"number of odd steps = {odd_steps}")
+print(f"number of even steps = {even_steps}")
